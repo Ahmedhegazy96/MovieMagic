671154555b2d210f5e15c46cda465392cc96f5a0
Ahmed Hegazy <ahmedhegz96@gmail.com>
2025-01-07 20:10:46 +0200

    Trending route done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
+import React from "react";
 import Logo from "./Logo";
+import Box from "./Box";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <Logo />
-      <p>�� 2021 MovieMagic. All rights reserved.</p>
-    </div>
+    <Box className="bg-gray-900 text-white py-8">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+        <Logo className="mb-4 md:mb-0" />
+        <div className="text-center md:text-right">
+          <p className="text-gray-400">
+            &copy; 2023 MovieMagic. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </Box>
   );
 }
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,20 @@
-export default function Logo() {
+import React from "react";
+import { Link } from "react-router-dom";
+import Box from "./Box";
+
+export default function Logo({ className }) {
   return (
-    <div className="flex items-center space-x-3 rtl:space-x-reverse h-8">
-      <span role="img" className="text-2xl" aria-label="popcorn">
-        🍿
-      </span>
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-        MovieMagic
-      </span>
-    </div>
+    <Link to="/" className="hover:no-underline">
+      <Box
+        className={`flex items-center space-x-2 p-2 bg-blue-700 border-2 border-blue-900 rounded-md shadow-lg transition-transform transform hover:scale-105 ${className}`}
+      >
+        <span role="img" className="text-2xl" aria-label="popcorn">
+          🍿
+        </span>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+          MovieMagic
+        </span>
+      </Box>
+    </Link>
   );
 }
